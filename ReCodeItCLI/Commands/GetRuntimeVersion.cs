ceb7bf8b3960eae86ae5126b64b83bc5ed1294a3
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-01-11 14:05:41 -0500

    add getruntimeversion command

---
+++ ReCodeItCLI/Commands/GetRuntimeVersion.cs
@@ -0,0 +1,22 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using ReCodeItLib.Utils;
+
+namespace ReCodeItCLI.Commands;
+
+[Command("GetRuntimeVersion", Description = "Prints out the .net runtime version this assembly targets")]
+public class GetRuntimeVersion : ICommand
+{
+	[CommandParameter(0, IsRequired = true, Description = "The absolute path to your dll.")]
+	public required string AssemblyPath { get; init; }
+	
+	public ValueTask ExecuteAsync(IConsole console)
+	{
+		var module = DataProvider.LoadModule(AssemblyPath);
+		
+		Logger.LogSync($"Target Runtime Version: {module.RuntimeVersion}");
+
+		return default;
+	}
+}

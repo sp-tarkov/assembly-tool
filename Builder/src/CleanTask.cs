4b56b61b07441d2485022b790725e81c9c051ffe
CWX <cwxdev@outlook.com>
2025-03-13 18:18:40 +0000

    Added Cake for building
    Benefits:
    - its C# and not PS
    - will work across all IDE's
    
    Notes:
    - used CakeFrosting over just CakeScripting so we dont have to deal with tool-manifests (this was an issue previously) and having to run dotnet cake from cli
    - Run Builder and that will build the projects, clean each obj/bin folder, clean de4dots folders and build the output folder
    - We might be able to build the builder and run the consoleapp from a postbuild script (might feel more normal from a devs standpoint)

---
+++ Builder/src/CleanTask.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Cake.Common.IO;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Frosting;
+using Path = System.IO.Path;
+
+namespace SptBuilder;
+
+[TaskName("CleanTask")]
+public class CleanTask : FrostingTask<Context>
+{
+    public override void Run(Context context)
+    {
+        CleanBuildFolders(context);
+        CleanDe4dotFolders(context);
+        CleanObjFolders(context);
+        CleanBinFolders(context);
+    }
+
+    private void CleanBuildFolders(Context context)
+    {
+        context.Log.Information("Cleaning Build Folders");
+        context.CleanDirectory(Path.Combine(context.Environment.WorkingDirectory.FullPath, "Build"));
+    }
+
+    private void CleanObjFolders(Context context)
+    {
+        context.Log.Information("Cleaning obj Folders");
+        // we do these per project folder to not try to delete the builder bin/obj
+        context.CleanDirectories(new GlobPattern("de4dot/**/obj"));
+        context.CleanDirectories(new GlobPattern("DumpLib/**/obj"));
+        context.CleanDirectories(new GlobPattern("ReCode*/**/obj"));
+    }
+
+    private void CleanBinFolders(Context context)
+    {
+        context.Log.Information("Cleaning bin Folders");
+        context.CleanDirectories(new GlobPattern("DumpLib/**/bin"));
+        context.CleanDirectories(new GlobPattern("ReCode*/**/bin"));
+    }
+
+    private void CleanDe4dotFolders(Context context)
+    {
+        context.Log.Information("Cleaning De4dot Folders");
+        context.CleanDirectory(Path.Combine(context.Environment.WorkingDirectory.FullPath, "de4dot", context.MsBuildConfiguration));
+    }
+}

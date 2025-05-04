1f37164632c937e3d61034d9e90d013be6fbdd65
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-05-04 12:35:49 -0400

    Start refactor to bring style inline with remapper

---
+++ AssemblyLib/AutoMatcher/Filters/FieldFilters.cs
@@ -0,0 +1,58 @@
+﻿using AsmResolver.DotNet;
+using AssemblyLib.Models;
+using AssemblyLib.Utils;
+
+namespace AssemblyLib.AutoMatcher.Filters;
+
+public class FieldFilters
+{
+    private static readonly List<string> FieldsToIgnore = DataProvider.Settings.FieldNamesToIgnore;
+    
+    public bool Filter(TypeDefinition target, TypeDefinition candidate, FieldParams fields)
+    {
+        // Target has no fields and type has no fields
+        if (!target.Fields.Any() && !candidate.Fields.Any())
+        {
+            fields.FieldCount = 0;
+            return true;
+        }
+		
+        // Target has fields but type has no fields
+        if (target.Fields.Any() && !candidate.Fields.Any()) return false;
+		
+        // Target has a different number of fields
+        if (target.Fields.Count != candidate.Fields.Count) return false;
+
+        var targetFields = GetFilteredFieldNamesInType(target)
+            .ToArray();
+		
+        var candidateFields = GetFilteredFieldNamesInType(candidate)
+            .ToArray();
+		
+        var commonFields = targetFields
+            .Intersect(candidateFields);
+		
+        // Fields in target that are not in candidate
+        var includeFields = targetFields
+            .Except(candidateFields);
+		
+        // Fields in candidate that are not in target
+        var excludeFields = candidateFields
+            .Except(targetFields);
+		
+        fields.IncludeFields.UnionWith(includeFields);
+        fields.ExcludeFields.UnionWith(excludeFields);
+		
+        fields.FieldCount = target.Fields.Count;
+		
+        return commonFields.Any();
+    }
+    
+    private static IEnumerable<string> GetFilteredFieldNamesInType(TypeDefinition type)
+    {
+        return type.Fields
+            // Don't match de-obfuscator given method names
+            .Where(m => !FieldsToIgnore.Any(mi => m.Name!.StartsWith(mi)))
+            .Select(s => s.Name!.ToString());
+    }
+}
---
+++ AssemblyLib/AutoMatcher/Filters/NestedFilters.cs
@@ -0,0 +1,54 @@
+﻿using AsmResolver.DotNet;
+using AssemblyLib.Models;
+
+namespace AssemblyLib.AutoMatcher.Filters;
+
+public class NestedFilters
+{
+    public bool Filter(TypeDefinition target, TypeDefinition candidate, NestedTypeParams nt)
+    {
+        // Target has no nt's but type has nt's
+        if (!target.NestedTypes.Any() && candidate.NestedTypes.Any())
+        {
+            nt.NestedTypeCount = 0;
+            return false;
+        }
+		
+        // Target has nt's but type has no nt's
+        if (target.NestedTypes.Any() && !candidate.NestedTypes.Any()) return false;
+		
+        // Target has a different number of nt's
+        if (target.NestedTypes.Count != candidate.NestedTypes.Count) return false;
+		
+        var commonNts = target.NestedTypes
+            .Select(s => s.Name)
+            .Intersect(candidate.NestedTypes.Select(s => s.Name));
+		
+        var includeNts = target.NestedTypes
+            .Select(s => s.Name!.ToString())
+            .Except(candidate.NestedTypes.Select(s => s.Name!.ToString()));
+		
+        var excludeNts = candidate.NestedTypes
+            .Select(s => s.Name!.ToString())
+            .Except(target.NestedTypes.Select(s => s.Name!.ToString()));
+		
+        nt.IncludeNestedTypes.UnionWith(includeNts);
+        nt.ExcludeNestedTypes.UnionWith(excludeNts);
+		
+        nt.NestedTypeCount = target.NestedTypes.Count;
+        nt.IsNested = target.IsNested;
+        nt.IsNestedAssembly = target.IsNestedAssembly;
+        nt.IsNestedFamily = target.IsNestedFamily;
+        nt.IsNestedPrivate = target.IsNestedPrivate;
+        nt.IsNestedPublic = target.IsNestedPublic;
+        nt.IsNestedFamilyAndAssembly = target.IsNestedFamilyAndAssembly;
+        nt.IsNestedFamilyOrAssembly = target.IsNestedFamilyOrAssembly;
+		
+        if (target.DeclaringType is not null)
+        {
+            nt.NestedTypeParentName = target.DeclaringType.Name!;
+        }
+		
+        return commonNts.Any() || target.NestedTypes.Count == 0;
+    }
+}

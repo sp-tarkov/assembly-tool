a2a78dfae6dbad579e8e7825775c2be2e13bcd9f
Cj <161484149+CJ-SPT@users.noreply.github.com>
2026-04-27 01:29:52 -0400

    Add disabled feature to transfer generic parameter names

--- AssemblyLib/DirectMapper/Renamers/SigBasedMemberRenamer.cs
+++ AssemblyLib/DirectMapper/Renamers/SigBasedMemberRenamer.cs
@@ -85,6 +85,7 @@ public class SigBasedMemberRenamer(
 
             RenameMethodsOnType(targetType, dummyType);
             RenamePropertiesOnType(targetType, dummyType);
+            //RenameGenericParametersOnType(targetType, dummyType);
         }
     }
 
@@ -262,6 +263,68 @@ public class SigBasedMemberRenamer(
                 targetProperty.Name = dummyProperty.Name;
                 dummyProperties.Remove(dummyProperty);
                 break;
+            }
+        }
+    }
+
+    private static void RenameGenericParametersOnType(TypeDefinition targetType, TypeDefinition dummyType)
+    {
+        RenameGenericParametersOnMethods(targetType, dummyType);
+        if (!targetType.HasGenericParameters || targetType.GenericParameters.Count != dummyType.GenericParameters.Count)
+        {
+            return;
+        }
+
+        for (var i = 0; i < targetType.GenericParameters.Count; i++)
+        {
+            var targetGenericParameter = targetType.GenericParameters[i];
+            var dummyGenericParameter = dummyType.GenericParameters[i];
+
+            if (targetGenericParameter.Name?.ToString() == dummyGenericParameter.Name?.ToString())
+            {
+                continue;
+            }
+
+            var oldName = targetGenericParameter.Name?.ToString();
+
+            targetGenericParameter.Name = dummyGenericParameter.Name;
+            Log.Information("Renamed generic param: {old} -> {new}", oldName, targetGenericParameter.Name?.ToString());
+        }
+    }
+
+    private static void RenameGenericParametersOnMethods(TypeDefinition targetType, TypeDefinition dummyType)
+    {
+        foreach (var targetMethod in targetType.Methods)
+        {
+            var dummyMethod = dummyType.Methods.FirstOrDefault(m => m.Name == targetMethod.Name);
+
+            if (
+                dummyMethod is null
+                || !targetMethod.HasGenericParameters
+                || targetMethod.GenericParameters.Count != dummyMethod.GenericParameters.Count
+            )
+            {
+                continue;
+            }
+
+            for (var i = 0; i < targetMethod.GenericParameters.Count; i++)
+            {
+                var targetGenericParameter = targetMethod.GenericParameters[i];
+                var dummyGenericParameter = dummyMethod.GenericParameters[i];
+
+                if (targetGenericParameter.Name?.ToString() == dummyGenericParameter.Name?.ToString())
+                {
+                    continue;
+                }
+
+                var oldName = targetGenericParameter.Name?.ToString();
+
+                targetGenericParameter.Name = dummyGenericParameter.Name;
+                Log.Information(
+                    "Renamed method generic param: {old} -> {new}",
+                    oldName,
+                    targetGenericParameter.Name?.ToString()
+                );
             }
         }
     }

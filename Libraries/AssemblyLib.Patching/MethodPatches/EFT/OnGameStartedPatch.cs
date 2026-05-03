a209ea80e4ee21b69cbfd7915b1befc26112ac0f
Cj <161484149+CJ-SPT@users.noreply.github.com>
2026-05-03 18:04:17 -0400

    refactor override

--- Libraries/AssemblyLib.Patching/MethodPatches/EFT/OnGameStartedPatch.cs
+++ Libraries/AssemblyLib.Patching/MethodPatches/EFT/OnGameStartedPatch.cs
@@ -8,8 +8,8 @@ public class OnGameStartedPatch : GameWorld
     /// <summary>
     ///     Sets the player scav's inventory as found in raid
     /// </summary>
-    [MethodPatch(typeof(GameWorld), nameof(GameWorld.OnGameStarted), MethodPatchType.Prefix)]
-    public override void OnGameStarted()
+    [MethodPatch(typeof(GameWorld), nameof(OnGameStarted), MethodPatchType.Prefix)]
+    public void Patch()
     {
         if (MainPlayer == null || MainPlayer.Profile.Side != EPlayerSide.Savage)
         {

237af0df94e35962302560f399b62047c6b36467
Cj <161484149+CJ-SPT@users.noreply.github.com>
2026-05-04 01:53:10 -0400

    port `FixPostScavRaidXpShowingZeroPatch`

--- Libraries/AssemblyLib.Patching/MethodPatches/EFT/GeneralFixes.cs
+++ Libraries/AssemblyLib.Patching/MethodPatches/EFT/GeneralFixes.cs
@@ -1,5 +1,8 @@
+using System;
 using AssemblyLib.Patching.Tool;
 using EFT;
+using EFT.Counters;
+using EFT.UI.SessionEnd;
 
 namespace AssemblyLib.Patching.MethodPatches.EFT;
 
@@ -13,5 +16,35 @@ public class GeneralFixes
     public bool DisableDiscardLimits()
     {
         return false;
+    }
+
+    /// <summary>
+    ///     Disables RMT check for discard limits
+    /// </summary>
+    /// <returns>false, no limits</returns>
+    [MethodPatch(
+        typeof(SessionResultExitStatus),
+        nameof(SessionResultExitStatus.Show),
+        MethodPatchType.Prefix,
+        typeof(Profile),
+        typeof(PlayerVisualRepresentation),
+        typeof(ESideType),
+        typeof(ExitStatus),
+        typeof(TimeSpan),
+        typeof(IEftSession),
+        typeof(bool)
+    )]
+    public void FixPostScavRaidXpShowingZeroPatch(Profile activeProfile, ESideType side)
+    {
+        if (activeProfile.Side == EPlayerSide.Savage)
+        {
+            side = ESideType.Savage; // Also set side to correct value (defaults to USEC/BEAR when playing as scav)
+            var xpGainedInSession = activeProfile.Stats.Eft.SessionCounters.GetAllInt(CounterTag.Exp);
+            activeProfile.Stats.Eft.TotalSessionExperience = (int)(
+                xpGainedInSession
+                * activeProfile.Stats.Eft.SessionExperienceMult
+                * activeProfile.Stats.Eft.ExperienceBonusMult
+            );
+        }
     }
 }
